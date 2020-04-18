1a7c324269f80c60d2266452aa2026744bfe13dc
arifinoid <rohmadarifin007@gmail.com>
2020-04-18 16:44:14 +0700

    Tidy up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
+import Layout from "./components/layout";
+import Card, { CardContainer } from "./components/card";
+import Pagination from "./components/pagination";
+
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const imagesPerPage = 12;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get(`${process.env.REACT_APP_API_URL}`);
+
+        setImages(res.data);
+        setLoading(false);
+      } catch (err) {
+        setLoading(false);
+        throw err;
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const indexOfLastImage = currentPage * imagesPerPage;
+  const indexOfFirstImage = indexOfLastImage - imagesPerPage;
+
+  const currentImages = images.slice(indexOfFirstImage, indexOfLastImage);
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      {loading ? (
+        <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
+      ) : (
+        <>
+          <h1 className="text-3xl text-center mx-auto mt-4">Lorem ipsum</h1>
+          <CardContainer>
+            {currentImages.map((image) => (
+              <Card key={image.id} data={image} />
+            ))}
+          </CardContainer>
+          <Pagination
+            imagesPerPage={imagesPerPage}
+            totalImages={images.length}
+            paginate={paginate}
+          />
+        </>
+      )}
+    </Layout>
   );
-}
+};
 
 export default App;
